b3f43629b66251f41521bb8321eb8cc607bb422d
Gerson Antonio Pais Whittembury <gerson.m.pais.-nd@disney.com>
2025-07-25 18:54:44 -0500

    feat: add TableDetails test file

--- apps/mui-ui/src/containers/TableDetails/__tests__/TableDetails.test.tsx
+++ apps/mui-ui/src/containers/TableDetails/__tests__/TableDetails.test.tsx
@@ -1,7 +1,8 @@
+
 import { ReactElement } from 'react';
 import { render, screen } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import TableDetails from '../index';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
@@ -11,21 +12,19 @@ const mockStore = configureStore({
   reducer: rootReducer,
 });
 
-const renderComponent = (component: ReactElement) => {
-  const encodedURL =
-    '/ds/DeltaLake/db/dataeng_qa/t/s3%3A%2F%2Fdataeng-data-test%2Fqa%2Fdata%2Factivation_services%2Fv1%2Freceipt_audit';
-
+const renderComponent = () => {
+  // This path matches the expected pattern in TableDetails
+  const initialPath = '/dataSource/DeltaLake/database/dataeng_qa/asset/s3%3A%2F%2Fdataeng-data-test%2Fqa%2Fdata%2Factivation_services%2Fv1%2Freceipt_audit';
   render(
     <Provider store={mockStore}>
       <MockedProvider>
-        <MemoryRouter initialEntries={[encodedURL]}>
-          {component}
-          {/* <Routes initialEntries={[encodedURL]}>
+        <MemoryRouter initialEntries={[initialPath]}>
+          <Routes>
             <Route
-              path={'/ds/:dataSource/db/:databaseName/t/:tableId'}
+              path="/dataSource/:dataSource/database/:databaseName/asset/:tableId"
               element={<TableDetails />}
             />
-          </Routes> */}
+          </Routes>
         </MemoryRouter>
       </MockedProvider>
     </Provider>
@@ -34,8 +33,7 @@ const renderComponent = (component: ReactElement) => {
 
 describe('TableDetails component', () => {
   it('should show the component', async () => {
-    renderComponent(<TableDetails />);
-
+    renderComponent();
     expect(screen.getByText('Loading Table Details')).toBeInTheDocument();
   });
 });
